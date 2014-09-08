c11911660ec610726729ba22bfe1da4a0c2c94e8
Lucas Stadler <lu@papill0n.org>
2014-09-08 21:21:37 +0200

    drop the "/default" suffix.
    that was quite unnecessary. this way it's quite easy to guess what
    something is.

--- detect/detect.go
+++ detect/detect.go
@@ -19,39 +19,39 @@ type Matcher func(string) bool
 type Commands map[string]string
 
 var ProjectTypes = []*Project{
-	&Project{"c/default", Commands{"run": "gcc -o $(basename {file} .c) {file} && ./$(basename {file} .c)"},
+	&Project{"c", Commands{"run": "gcc -o $(basename {file} .c) {file} && ./$(basename {file} .c)"},
 		matchPattern("*.c")},
 	&Project{"clojure/leiningen", Commands{"build": "lein uberjar", "run": "lein run", "test": "lein test"},
 		matchFile("project.clj")},
-	&Project{"coffeescript/default", Commands{"run": "coffee {file}"}, matchPattern("*.coffee")},
+	&Project{"coffeescript", Commands{"run": "coffee {file}"}, matchPattern("*.coffee")},
 	&Project{"docker/fig", Commands{"build": "fig build", "run": "fig up"}, matchFile("fig.yml")},
-	&Project{"docker/default", Commands{"build": "docker build ."}, matchFile("Dockerfile")},
-	&Project{"executable", Commands{"run": "{file}"}, executableDefault},
-	&Project{"go/default", Commands{"build": "go build {file}", "run": "go build $(basename {file}) && ./$(basename {file} .go)",
+	&Project{"docker", Commands{"build": "docker build ."}, matchFile("Dockerfile")},
+	&Project{"executable", Commands{"run": "{file}"}, executable},
+	&Project{"go", Commands{"build": "go build {file}", "run": "go build $(basename {file}) && ./$(basename {file} .go)",
 		"test": "go test"}, matchPattern("*.go")},
 	&Project{"haskell/cabal", Commands{"build": "cabal sandbox init && cabal install --only-dependencies && cabal build",
 		"run": "cabal sandox init && cabal run", "test": "cabal sandbox init && cabal test"}, matchPattern("*.cabal")},
-	&Project{"haskell/default", Commands{"run": "runhaskell {file}"}, haskellDefault},
-	&Project{"idris/default", Commands{"run": "idris -o $(basename {file} .idr) {file} && ./$(basename {file} .idr)"},
+	&Project{"haskell", Commands{"run": "runhaskell {file}"}, haskell},
+	&Project{"idris", Commands{"run": "idris -o $(basename {file} .idr) {file} && ./$(basename {file} .idr)"},
 		matchPattern("*.idr")},
 	&Project{"java/maven", Commands{"build": "mvn compile", "test": "mvn compile test"}, matchFile("pom.xml")},
 	&Project{"javascript/npm", Commands{"build": "npm install", "run": "npm start", "test": "npm test"},
 		matchFile("package.json")},
 	&Project{"javascript/meteor", Commands{"run": "meteor"}, matchFile(".meteor/.id")},
-	&Project{"javascript/default", Commands{"run": "node {file}"}, matchPattern("*.js")},
-	&Project{"jekyll", Commands{"build": "jekyll build", "run": "jekyll serve --watch"}, jekyllDefault},
-	&Project{"julia/default", Commands{"run": "julia {file}"}, matchPattern("*.jl")},
-	&Project{"latex/default", Commands{"run": "pdflatex {file}"}, latexDefault},
+	&Project{"javascript", Commands{"run": "node {file}"}, matchPattern("*.js")},
+	&Project{"jekyll", Commands{"build": "jekyll build", "run": "jekyll serve --watch"}, jekyll},
+	&Project{"julia", Commands{"run": "julia {file}"}, matchPattern("*.jl")},
+	&Project{"latex", Commands{"run": "pdflatex {file}"}, latex},
 	&Project{"python/django", Commands{"build": "python manage.py syncdb", "run": "python manage.py runserver",
 		"test": "python manage.py test"}, matchFile("manage.py")},
-	&Project{"python/default", Commands{"run": "python {file}"}, matchPattern("*.py")},
+	&Project{"python", Commands{"run": "python {file}"}, matchPattern("*.py")},
 	&Project{"ruby/rails", Commands{"build": "bundle exec rake db:migrate", "run": "rails server",
 		"test": "bundle exec rake test"}, matchFile("bin/rails")},
 	&Project{"ruby/rake", Commands{"run": "rake", "test": "rake test"}, matchFile("Rakefile")},
-	&Project{"ruby/default", Commands{"run": "ruby {file}"}, matchPattern("*.rb")},
+	&Project{"ruby", Commands{"run": "ruby {file}"}, matchPattern("*.rb")},
 	&Project{"rust/cargo", Commands{"build": "cargo build", "run": "cargo run", "test": "cargo test"},
 		matchFile("Cargo.toml")},
-	&Project{"rust/default", Commands{"run": "rustc {file} && ./$(basename {file} .rs)"}, matchPattern("*.rs")},
+	&Project{"rust", Commands{"run": "rustc {file} && ./$(basename {file} .rs)"}, matchPattern("*.rs")},
 	&Project{"cmake", Commands{"build": "mkdir .build && cd .build && cmake .. && make"}, matchFile("CMakeLists.txt")},
 	&Project{"make", Commands{"run": "make", "test": "make test"}, matchFile("Makefile")},
 	&Project{"procfile", Commands{"run": "$(sed -n 's/^web: //p' Procfile)"}, matchFile("Procfile")},
@@ -116,18 +116,18 @@ func matchFile(fileName string) Matcher {
 	}
 }
 
-func executableDefault(file string) bool {
+func executable(file string) bool {
 	return fileutil.IsExecutable(file)
 }
 
-func haskellDefault(file string) bool {
+func haskell(file string) bool {
 	return matchingFileOrDir(file, "*.hs") || matchingFileOrDir(file, "*.lhs")
 }
 
-func jekyllDefault(file string) bool {
+func jekyll(file string) bool {
 	return hasFile(file, "_config.yml") || fileutil.IsDir(fileutil.Join(file, "_posts"))
 }
 
-func latexDefault(file string) bool {
+func latex(file string) bool {
 	return matchingFileOrDir(file, "*.latex") || matchingFileOrDir(file, "*.tex")
 }
